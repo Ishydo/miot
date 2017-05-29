ba6cbd28c9d799c2988d6a9931cc7ea7aef84f78
Dom <ducommun.dom@gmail.com>
2017-05-29 15:46:40 +0200

    Updated urls for allauth

--- miot/urls.py
+++ miot/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from miot.views import poi_views, profile_views
+from miot.views import poi_views, profile_views, static_views
 
 urlpatterns = [
     url(r'^places', poi_views.PointOfInterestListView.as_view(), name="poi_list"),
     url(r'^p/(?P<slug>[-\w]+)/', poi_views.PointOfInterestDetailView.as_view(), name="poi_detail"),
-    url(r'^dashboard', profile_views.Dashboard.as_view(), name="dashboard")
+    url(r'^dashboard', profile_views.Dashboard.as_view(), name="dashboard"),
+    url(r'^$', static_views.HomepageView.as_view(), name="homepage")
 ]
--- miotProject/urls.py
+++ miotProject/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import logout
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^', include('miot.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
+    url(r'^accounts/', include('allauth.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
