99d1098aefd8ba5c290f1200b052904ed2cb23a4
Dom <ducommun.dom@gmail.com>
2017-05-29 14:15:33 +0200

    Added view for profile and templates

--- miot/urls.py
+++ miot/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from miot.views import poi_views
+from miot.views import poi_views, profile_views
 
 urlpatterns = [
     url(r'^places', poi_views.PointOfInterestListView.as_view(), name="poi_list"),
     url(r'^p/(?P<slug>[-\w]+)/', poi_views.PointOfInterestDetailView.as_view(), name="poi_detail"),
+    url(r'^dashboard', profile_views.Dashboard.as_view(), name="dashboard")
 ]
---
+++ miot/views/profile_views.py
@@ -0,0 +1,11 @@
+
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from miot.models import PointOfInterest, Profile
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    def get(self, request):
+        context, context["pois"] = {}, request.user.profile.fetchPointOfInterests()
+        return render(request, "dashboard/dashboard.html", context)
