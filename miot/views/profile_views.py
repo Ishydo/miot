3afc25cda1f8948ab131676b5c148d6914c307a7
Dom <ducommun.dom@gmail.com>
2017-05-30 15:38:47 +0200

    Dashboard base organisation and style

--- miot/views/profile_views.py
+++ miot/views/profile_views.py
@@ -3,10 +3,11 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from miot.models import PointOfInterest, Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.conf import settings
 
 class Dashboard(LoginRequiredMixin, TemplateView):
     def get(self, request):
         context, context["pois"] = {}, request.user.profile.fetchPointOfInterests()
         context["pages"] = request.user.profile.fetchPages()
+        context["API_KEY"] = settings.MAP_WIDGETS["GOOGLE_MAP_API_KEY"]
         return render(request, "dashboard/dashboard.html", context)
