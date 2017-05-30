e1ec6d47764ba6f35a819d2b07103d1e0a028c47
Dom <ducommun.dom@gmail.com>
2017-05-30 09:28:24 +0200

    Added success message and redirection

--- miot/views/poi_views.py
+++ miot/views/poi_views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from miot.models import PointOfInterest
+from miot.forms import PointOfInterestForm
 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PointOfInterestListView(ListView):
     model = PointOfInterest
@@ -10,3 +13,9 @@ class PointOfInterestDetailView(DetailView):
     model = PointOfInterest
     template_name = "poi_detail.html"
     context_object_name = "poi"
+
+class PointOfInterestCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    form_class=PointOfInterestForm
+    template_name = "dashboard/poi_form.html"
+    success_url="/dashboard"
+    success_message = "%(name)s was created successfully"
