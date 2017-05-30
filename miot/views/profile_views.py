8a70d09a7366140e29de6f499dcd49bcddb02067
Dom <ducommun.dom@gmail.com>
2017-05-30 09:29:40 +0200

    Minor changes on profileviews

--- miot/views/profile_views.py
+++ miot/views/profile_views.py
@@ -8,4 +8,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class Dashboard(LoginRequiredMixin, TemplateView):
     def get(self, request):
         context, context["pois"] = {}, request.user.profile.fetchPointOfInterests()
+        context["pages"] = request.user.profile.fetchPages()
         return render(request, "dashboard/dashboard.html", context)
