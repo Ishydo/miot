62d2f01c7caaa579a75df3058aa8c31a73b6c76a
Dom <ducommun.dom@gmail.com>
2017-06-05 21:01:20 +0200

    Added tinymce and style adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,18 @@ import './style.css'
 import 'jquery-tags-input/dist/jquery.tagsinput.min.css'
 import 'jquery-tags-input/dist/jquery.tagsinput.min.js'
 import './tags.js'
+
+// Core - these two are required :-)
+import tinymce from 'tinymce/tinymce.min.js'
+import 'tinymce/themes/modern/theme.min.js'
+import 'tinymce/skins/lightgray/skin.min.css'
+
+tinymce.init({
+  selector: ".tinymce-editor",
+  skin: false,
+  setup: function (editor) {
+    editor.on('change', function () {
+        editor.save();
+    });
+}
+});
