b15f504c682935ce9df300efa8ce3f0112f6103f
Dom <ducommun.dom@gmail.com>
2017-06-05 21:01:37 +0200

    Urls adjustments

--- miot/urls.py
+++ miot/urls.py
@@ -5,10 +5,7 @@ from miot.views import page_views, poi_views, profile_views, static_views
 
 urlpatterns = [
     url(r'^places', poi_views.PointOfInterestListView.as_view(), name="poi_list"),
-    url(r'^p/(?P<slug>[-\w]+)/', poi_views.PointOfInterestDetailView.as_view(), name="poi_detail"),
-    url(r'^dashboard$', profile_views.Dashboard.as_view(), name="dashboard"),
-    url(r'^$', static_views.HomepageView.as_view(), name="homepage"),
-
+    url(r'^dashboard/$', profile_views.Dashboard.as_view(), name="dashboard"),
     url(r'^dashboard/new/pointofinterest', poi_views.PointOfInterestCreateView.as_view(), name="poi_create"),
     url(r'^dashboard/new/page/(?P<slug>[-\w]+)', page_views.PageCreateView.as_view(), name="page_create"),
     url(r'^dashboard/new/page', poi_views.PointOfInterestSelectView.as_view(), name="poi_select"),
@@ -16,6 +13,7 @@ urlpatterns = [
     url(r'^dashboard/delete/page/(?P<slug>[-\w]+)', page_views.PageDeleteView.as_view(), name="page_delete"),
     url(r'^dashboard/edit/page/(?P<slug>[-\w]+)', page_views.PageUpdateView.as_view(), name="page_update"),
     url(r'^dashboard/edit/pointofinterest/(?P<slug>[-\w]+)', poi_views.PointOfInterestUpdateView.as_view(), name="poi_update"),
-
+    url(r'^p/(?P<slug>[-\w]+)/', poi_views.PointOfInterestDetailView.as_view(), name="poi_detail"),
+    url(r'^$', static_views.HomepageView.as_view(), name="homepage"),
 
 ]
