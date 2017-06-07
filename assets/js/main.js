e368bd82fa6a8509e810ecde622c2da124059518
Dom <ducommun.dom@gmail.com>
2017-06-07 08:47:53 +0200

    Sortable in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,6 @@ tinymce.init({
     });
 }
 });
+
+
+import 'sortablejs'
--- miot/views/page_views.py
+++ miot/views/page_views.py
@@ -1,10 +1,25 @@
-from django.views.generic import CreateView, DeleteView, UpdateView
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView, View
 from miot.models import Page, PointOfInterest
 from miot.forms import PointOfInterestForm, PageForm
+from django.http import HttpResponse
+from django.db.models import F
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.decorators.csrf import csrf_exempt
+
+class PoiPageUpdateView(LoginRequiredMixin, ListView):
+    model = PointOfInterest
+    template_name = "dashboard/manage-poi-pages.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PoiPageUpdateView, self).get_context_data(**kwargs)
+        context['poi'] = PointOfInterest.objects.get(slug=self.kwargs['slug'])
+        return context
+
+    def get_queryset(self):
+        return Page.objects.filter(poi=PointOfInterest.objects.get(slug=self.kwargs['slug'])).order_by("index")
 
 class PageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     form_class=PageForm
@@ -14,6 +29,7 @@ class PageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.poi = PointOfInterest.objects.get(slug=self.kwargs["slug"])
+        form.instance.index = Page.objects.filter(poi=form.instance.poi).count()
         return super(PageCreateView, self).form_valid(form)
 
 class PageUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
@@ -32,3 +48,16 @@ class PageDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
         return super(PageDeleteView, self).delete(request, *args, **kwargs)
+
+
+class ChangeOrder(View):
+    def post(self, request, *args, **kwargs):
+        oldIndex = request.POST.get("oldIndex")
+        newIndex = request.POST.get("newIndex")
+
+        if newIndex > oldIndex:
+            ps = Page.objects.filter(poi=PointOfInterest.objects.get(pk=request.POST.get("poi")), index__lte=newIndex)
+            ps.update(index= F('index') - 1)
+        else:
+            pass
+        return HttpResponse("ok")
