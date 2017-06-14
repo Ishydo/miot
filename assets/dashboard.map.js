8b4f2195e02d0b4672aa661e021a0cf49ce9ac4f
Dom <ducommun.dom@gmail.com>
2017-06-14 14:23:04 +0200

    Added marker cluster support on dashboard

--- assets/dashboard.map.js
+++ assets/dashboard.map.js
@@ -1,9 +1,14 @@
 
-var GoogleMapsLoader = require('google-maps'); // only for common js environments
+// For map clustering
+import "js-marker-clusterer"
+
+// Google maps loader
+var GoogleMapsLoader = require('google-maps');
 GoogleMapsLoader.KEY = 'AIzaSyCWAz__u7xGDOZX7UI_daXZbQqZlPhFC4k';
 
 var myLatLng = {lat: -25.363, lng: 131.044};
 
+// Map initialisation
 GoogleMapsLoader.load(function(google) {
   var map = new google.maps.Map(document.getElementById("map-canvas"), {
     zoom: 4,
@@ -13,33 +18,31 @@ GoogleMapsLoader.load(function(google) {
     zoomControl: true
   });
 
-  var bounds = new google.maps.LatLngBounds();
+  // Bounds, in page markers and empty array
+  var bounds = new google.maps.LatLngBounds();    // Bounds for auto bounding map
+  var markers = $.parseJSON($("#markers").text()) // Markers in DOM (JSON in div)
+  var markersArr = [];                            // Marker objects array
 
-  var markers = $.parseJSON($("#markers").text())
-
-
+  // For each marker in page
   $.each(markers, function(index, marker) {
 
-    var info = new google.maps.InfoWindow({
-      content: '<h6>' + marker.name + '</h6>',
-      maxWidth: 300
-    });
-
+    // Set position
     var pos = new google.maps.LatLng(marker.position.lat, marker.position.lon)
+
+    // Create the marker on the map
+    var gMarker = new google.maps.Marker({position: pos, map: map,});
+
+    // Create the info bubble
+    var info = new google.maps.InfoWindow({content: '<h6>' + marker.name + '</h6>', maxWidth: 300,});
+
+    // Listener on click on marker
+    gMarker.addListener('click', function(){info.open(map, gMarker);});
+
+    // Extends bounds and push marker
     bounds.extend(pos);
+    markersArr.push(gMarker);
 
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map,
-    });
-
-    marker.addListener('click', function(){
-      info.open(map, marker);
-    });
   });
-
-  map.fitBounds(bounds);
-
 
     var styledMapType = new google.maps.StyledMapType([
     {
@@ -72,5 +75,12 @@ GoogleMapsLoader.load(function(google) {
   //Associate the styled map with the MapTypeId and set it to display.
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
+
+  // Auto fit bounds
+  map.fitBounds(bounds);
+
+  // Marker clustering
+  var mcOptions = {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'};
+  var mc = new MarkerClusterer(map, markersArr, mcOptions);
 
 });
