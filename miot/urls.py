3f4d6ed009d3a640196a3ef984dc369f790d0b1e
Dom <ducommun.dom@gmail.com>
2017-06-16 11:31:31 +0200

    Search functionality on single page

--- miot/urls.py
+++ miot/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from miot.views import page_views, poi_views, profile_views, static_views
 
 urlpatterns = [
-    url(r'^places$', poi_views.PointOfInterestListView.as_view(), name="poi_list"),
+    url(r'^places$', poi_views.PointOfInterestListViewPos.as_view(), name="poi_list"),
     url(r'^places/(\d+(?:\.\d+)?)/(\d+(?:\.\d+)?)$', poi_views.PointOfInterestListViewPos.as_view(), name="poi_list_pos"),
+    url(r'^places/(\d+(?:\.\d+)?)/(\d+(?:\.\d+)?)/(?P<query>[-\w]+)$', poi_views.PointOfInterestListViewPos.as_view(), name="search"),
 
     url(r'^dashboard/$', profile_views.Dashboard.as_view(), name="dashboard"),
     url(r'^dashboard/new/pointofinterest', poi_views.PointOfInterestCreateView.as_view(), name="poi_create"),
--- miot/views/poi_views.py
+++ miot/views/poi_views.py
@@ -8,8 +8,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 
+from django.db.models import Q
+
 from hitcount.views import HitCountDetailView
 from hitcount.models import HitCount
+
+from functools import reduce
 
 class PointOfInterestDiscoverView(ListView):
     model = PointOfInterest
@@ -26,13 +30,33 @@ class PointOfInterestListView(ListView):
         context['object_list'] = PointOfInterest.objects.filter(active=True)
         return context
 
-class PointOfInterestListViewPos(TemplateView):
-    def get(self, request, lat=None, lon=None):
-        context = {}
+class PointOfInterestListViewPos(ListView):
+    paginate_by = 4
+    template_name="poi_list_pos.html"
+    model = PointOfInterest
+
+    def get_queryset(self):
+        result = super(PointOfInterestListViewPos, self).get_queryset()
+        if self.request.GET.get("q") is not None and self.request.GET.get("q") != "":
+            query = self.request.GET.get("q")
+            query_list = query.split()
+            result = PointOfInterest.objects.filter(
+                       reduce(lambda x, y: x | y, [Q(name__icontains=word) for word in query_list]) |
+                       (Q(tags__name__in=query_list))
+                       ).distinct()
+            return result
+        else:
+            return PointOfInterest.objects.filter(active=True)
+
+
+    def get_context_data(self, **kwargs):
+        context = super(PointOfInterestListViewPos, self).get_context_data(**kwargs) # get the default context data
         context["bestPois"] = sorted(PointOfInterest.objects.filter(active=True)[:3], key=lambda p: p.hit_count.hits, reverse=True)
-        context["nearPois"] = get_near_poi(lat, lon)
-        context['object_list'] = PointOfInterest.objects.filter(active=True)
-        return render(request, "poi_list_pos.html", context)
+        if self.request.GET.get("lat") is not None:
+            context["nearPois"] = get_near_poi(self.request.GET.get("lat"), self.request.GET.get("lon"))
+        if self.request.GET.get("q") is not None:
+            context["search"] = True
+        return context
 
 class PointOfInterestManageListView(ListView):
     model = PointOfInterest
